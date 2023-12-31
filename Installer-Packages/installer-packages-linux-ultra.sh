6b71f20f834c783cc3cb0580ee0d33baa78f1d79
Alessandro.M <123805284+AlessandroMagl@users.noreply.github.com>
2023-12-31 17:25:32 +0100

    Update installer-packages-linux-ultra.sh

--- Installer-Packages/installer-packages-linux-ultra.sh
+++ Installer-Packages/installer-packages-linux-ultra.sh
@@ -15,6 +15,6 @@ sudo apt install -y unzip curl bash sudo
 # Other
 sudo apt install net-tools
 curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bash
-
+sudo apt install git
 
 echo "Utility and Essential packages and commands installed successfully."
