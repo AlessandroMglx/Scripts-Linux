9264c1b40b2bf7b50adaafbc0e4f0e4ecca26efe
Alessandro.M <123805284+AlessandroMagl@users.noreply.github.com>
2023-12-31 17:39:44 +0100

    Update installer-packages-linux-ultra.sh

--- Installer-Packages/installer-packages-linux-ultra.sh
+++ Installer-Packages/installer-packages-linux-ultra.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Essentials
+cd
 apt install sudo
 
 # Update package lists
@@ -18,5 +19,8 @@ curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | bas
 sudo apt install speedtest-cli
 sudo apt upgate && apt upgrade
 sudo apt install git
+
+# End
+rm installer-packages-linux.sh
 
 echo "Utility and Essential packages and commands installed successfully."
